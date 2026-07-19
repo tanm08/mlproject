f2ba1694768fe4ac0299dbc98e827848a115e7be
Tanmay Nagare <tanmaynagare2204@gmail.com>
2026-07-19 22:43:51 +0530

    Some Chnages done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -42,6 +42,6 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__=="__main__":
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
+# if __name__=="__main__":
+#     obj=DataIngestion()
+#     obj.initiate_data_ingestion()
